b83539e64e96165830a28ff9a3681519dd9a6008
Yoann MOUGNIBAS <yoann@mougnibas.fr>
2018-07-28 00:08:05 +0200

    FIXME tomorrow...

--- shoplist/contract/src/main/java/fr/mougnibas/cookhelper/shoplist/contract/util/EndpointsReader.java
+++ shoplist/contract/src/main/java/fr/mougnibas/cookhelper/shoplist/contract/util/EndpointsReader.java
@@ -58,6 +58,7 @@ public final class EndpointsReader {
   private EndpointsReader() {
 
     // Get a resource bundle
+    // FIXME Rename "endpoints.properties" to "endpoints-recipe.properties"
     ResourceBundle rb = ResourceBundle.getBundle("endpoints");
 
     // Get server host data
