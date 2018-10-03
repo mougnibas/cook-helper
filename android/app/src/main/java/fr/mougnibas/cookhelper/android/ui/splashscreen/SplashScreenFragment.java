0d7120e47e67bd90c52772da155e47580c84f8b8
Yoann MOUGNIBAS <yoann@mougnibas.fr>
2018-10-03 19:39:49 +0200

    trying to use good practices

--- android/app/src/main/java/fr/mougnibas/cookhelper/android/ui/splashscreen/SplashScreenFragment.java
+++ android/app/src/main/java/fr/mougnibas/cookhelper/android/ui/splashscreen/SplashScreenFragment.java
@@ -5,13 +5,20 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import fr.mougnibas.cookhelper.R;
+import fr.mougnibas.cookhelper.android.SplashScreenActivity;
 
 public class SplashScreenFragment extends Fragment {
+
+    /**
+     * Class tag, for logging.
+     */
+    private static final String TAG = SplashScreenFragment.class.getName();
 
     private SplashScreenViewModel mViewModel;
 
@@ -23,12 +30,19 @@ public class SplashScreenFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        // Some logging
+        Log.i(TAG, "onCreateView");
+
         return inflater.inflate(R.layout.splash_screen_fragment, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        // Some logging
+        Log.i(TAG, "onActivityCreated");
+
         mViewModel = ViewModelProviders.of(this).get(SplashScreenViewModel.class);
         // TODO: Use the ViewModel
     }
